8e82a29ae9f55d8802f5418a611cd329f517c74a
Mezali <danieldelimamazali@hotmail.com>
2023-07-27 16:08:49 -0300

    error 500 page

--- aplication/routes.py
+++ aplication/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from pymongo.errors import PyMongoError
 
 from aplication import app, mongo
 
@@ -10,7 +11,10 @@ def index():
 
 @app.route('/market')
 def market():
-    items_cursor = mongo.db.item.find()
-    items = list(items_cursor)
-
-    return render_template('market.html', items=items)
+    try:
+        items_cursor = mongo.db.item.find()
+        items = list(items_cursor)
+        return render_template('market.html', items=items)
+    except PyMongoError:
+        message = 'An unexpected error occur while fetching data from the database.'
+        return render_template('error.html', message=message), 500
