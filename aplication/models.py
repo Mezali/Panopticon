65ccd49bb6b2306f7bbc85a1115f5d2705be7991
Mezali <danieldelimamazali@hotmail.com>
2023-07-28 14:28:25 -0300

    and again, ch-ch-ch changes yeah

--- aplication/models.py
+++ aplication/models.py
@@ -1,8 +1,17 @@
-
-class Item:
-    def __int__(self, _id, name, barcode, description, price):
+class User:
+    def __int__(self, _id, username, email, password_hash):
         self.id = _id
-        self.name = name
-        self.barcode = barcode
-        self.description = description
-        self.price = price
+        self.username = username
+        self.email = email
+        self.password_hash = password_hash
+
+
+class NVR:
+    def __int__(self, location, ip, model, serial_number, users, channels, cameras):
+        self.ip = ip
+        self.location = location
+        self.model = model
+        self.serial_number = serial_number
+        self.users = users
+        self.channels = channels
+        self.cameras = cameras
